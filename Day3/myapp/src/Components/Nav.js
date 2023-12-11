8affad3af4820a489b158b2a494388d92c3922da
Shreyas248 <shreyaspp248@gmail.com>
2023-12-11 16:28:34 +0530

    Day 3

---
+++ Day3/myapp/src/Components/Nav.js
@@ -0,0 +1,41 @@
+// // -------------------------Arrow Function-----------------------------------
+// const Nav =()=> {
+//     return(
+//         <>
+//         <div className="navbar">
+//             Hello Word
+//         </div>
+//         </>
+//     )
+// }
+
+// export default Nav;
+
+// //--------------using function Nav() Method-------------------------------
+
+// function Nav(){
+//         return(
+//             <>
+//             <div>
+//                 Arrow function
+//             </div>
+//             </>
+//         )
+//     }
+    
+// export default Nav;
+
+const Nav = () => {
+    return (
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Nav;
---
+++ Day3/myapp/src/Components/home.js
@@ -0,0 +1,10 @@
+const home=()=>{
+    return(
+        <>
+        <p>
+        <h1>Welcome to My React Home Page</h1>
+        </p>
+        </>
+    )
+}
+export default home
