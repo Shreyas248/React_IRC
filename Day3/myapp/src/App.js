b096b12f52df0c334bba62513e12af620946b76b
Shreyas248 <shreyaspp248@gmail.com>
2023-12-12 16:25:17 +0530

    Day4

--- Day3/myapp/src/App.js
+++ Day3/myapp/src/App.js
@@ -1,15 +1,19 @@
-import Nav from "./Components/Nav";
-import home from "./Components/home";
-// import Footer from "./Components/Footer";
-import './Assets/Css/Nav.css';
-import "./Components/home";
-import './Assets/Css/home.css';
-function App(){
-  return(
+import React from 'react';
+import Navbar from './Components/Nav'
+import Home from './Components/Home';
+import Footer from './Components/Footer'
+import './assets/css/Footer.css';
+import './assets/css/Nav.css';
+import './assets/css/Home.css'
+
+const App = () => {
+  return (
     <div>
-    <Nav/>
-    <h1 class="head">CHAINSAW MAN</h1>
+      <Navbar />
+      <Home />
+      <Footer/>
     </div>
-  )
-}
+  );
+};
+
 export default App;
