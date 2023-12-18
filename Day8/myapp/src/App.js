33d31f23185f856d0ae72af400d399a7d98b3750
Shreyas248 <shreyaspp248@gmail.com>
2023-12-18 14:38:28 +0530

    Day8

---
+++ Day8/myapp/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import WithLoading from './components/WithLoading'
+
+// const WrappedComponentWithLoading = WithLoading(MyComponent)
+ const App = () => {
+  return (
+    <div></div>
+  )
+}
+
---
+++ Day8/myapp/src/components/WithLoading.js
@@ -0,0 +1,20 @@
+// import React, {useEffect, useState} from 'react'
+// const WithLoading = ({WrappedComponent}) =>{
+//     return function WithLoadingx(props){
+//         const [isLoading, setIsLoading] = useState(true)
+
+//         useEffect (()=>{
+//             setTimeout(()=>{
+//                 setIsLoading(false)
+//             },2000)
+
+//         },[])
+//         return isLoading ?(
+//             <>
+//             Loading
+//             </>) :(
+//                 <WrappedComponent {...props}/>
+//             )
+//     }
+// }
+// export default WithLoading
